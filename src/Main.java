072dad2d42a6ed566ee94d0b37511b4b04fd65d3
Frederick <fredeoscawits5@gmail.com>
2023-11-01 13:16:54 -0600

    [ADD]Radix sort algorithm
    The code was based from the one provided by the website 'GeeksForGeeks'

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ import lib.*;
 
 public class Main{
     public static void main(String[] args){
-
+        int[] listToSort = {21, 807, 101, 57, 40, 7, 2};
+        int[] sorted = RadixSort.sort(listToSort);
+        for (int num:sorted){
+            System.out.print(num + ", ");
+        }
+        System.out.println("\n");
+        
     }
 }
