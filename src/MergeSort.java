41d52c1919962da5cf27a7592682ea7af40b24aa
EmMongeHnz <133249327+EmMongeHnz@users.noreply.github.com>
2023-11-03 00:31:54 -0600

    Comments and credits

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -2,31 +2,42 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+/* 
+Code based on a python version made by ChioCode: https://www.youtube.com/watch?v=ACFZn_xQcz8
+*/
+
 public class MergeSort {
 
+    // This function appends an element based on the index to which the array has been populated.
     public static int[] append (int[] array, int element, int index) {
         array[index] = element;
         return array;
     }
 
+    // This function is the main Function of the class to sort an array using Merge sort.
     public static int[] sort (int[] arrayToSort) {
         return mergeSort(arrayToSort);
     }
+
+    // This is the main function to split the array in halfs until they are lenght one and then, merge them with the "Merge" function.
     private static int[] mergeSort(int[] array) {
         if (array.length == 1) {
             return array;
         }
 
+        // Here the arrays are divided.
         int middle = array.length/2;
         int[] leftArray = Arrays.copyOfRange(array, 0, middle);
         int[] rightArray = Arrays.copyOfRange(array, middle, array.length);
 
+        
         int[] sortedLeft = mergeSort(leftArray);
         int[] sortedRight = mergeSort(rightArray);
 
         return Merge(sortedLeft, sortedRight);
     }
 
+    // This function takes all the individual arrays to merge them in one and finally return the result (sorted array).
     private static int[] Merge(int[] leftArray, int[] rightArray) {
         int index = 0;
         int[] result = new int[leftArray.length + rightArray.length];
